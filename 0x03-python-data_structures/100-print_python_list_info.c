beda9fc88d39b82643c1ea44f14ed252ebf10b7c
Kelys3 <fredatd2016@gmail.com>
2023-07-18 02:03:18 +0000

    more updates

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Python.h>
+#include "listobject.h"
+#include "object.h"
 /**
  * print_python_list_info - Print some basic info about Python lists
  * @p: PyObject
