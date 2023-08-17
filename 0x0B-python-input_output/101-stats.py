b753b74c45d92828ffe309c1b30c9d6b895aa550
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-08-17 00:16:53 -0700

    code correction

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,50 +1,49 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics
-"""
+"""A script that reads stdin line by line and computes metrics"""
 
 
-def print_stats(size, status_codes):
-    """Print accumulated metrics
-    """
+def read_stdin(size, status_codes):
+    """Displaying the  computed metrics"""
+
     print("File size: {}".format(size))
-    for key in sorted(status_codes):
-        print("{}: {}".format(key, status_codes[key]))
+    for elems in sorted(status_codes):
+        print("{}: {}".format(elems, status_codes[elems]))
 
 
 if __name__ == "__main__":
     import sys
 
     size = 0
+    total = 0
     status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+    possibilities = ['200', '301', '400', '401', '403', '404', '405', '500']
 
     try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
+        for row in sys.stdin:
+            if total == 10:
+                read_stdin(size, status_codes)
+                total = 1
             else:
-                count += 1
+                total = total + 1
 
-            line = line.split()
+            row = row.split()
 
             try:
-                size += int(line[-1])
+                size += int(row[-1])
             except (IndexError, ValueError):
                 pass
 
             try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
+                if row[-2] in possibilities:
+                    if status_codes.get(row[-2], -1) == -1:
+                        status_codes[row[-2]] = 1
                     else:
-                        status_codes[line[-2]] += 1
+                        status_codes[row[-2]] += 1
             except IndexError:
                 pass
 
-        print_stats(size, status_codes)
+        read_stdin(size, status_codes)
 
     except KeyboardInterrupt:
-        print_stats(size, status_codes)
+        read_stdin(size, status_codes)
         raise
