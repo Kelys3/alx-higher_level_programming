a634e24aed4271b9627bf0b16320c3ad4b98bc9e
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-10-16 07:57:49 -0700

    task2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv.length;
 
-if (argv.length === 2) {
+if (argv === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (argv === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
