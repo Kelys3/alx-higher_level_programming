c2c301da0fd8076ab216460cea47c639bbfbf90d
Kelys3 <fredatd2016@gmail.com>
2023-11-02 22:48:33 -0700

    advanced

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me to get the response "You got me!"
-curl -sL -X PUT -H "Origin: ALX" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: School" -d "user_id=98" 0.0.0.0:5000/catch_me
