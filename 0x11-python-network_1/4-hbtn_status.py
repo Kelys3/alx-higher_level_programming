4d3fa1dde64a05b872ebb9928add36b05ca37ebe
Kelys3 <fredatd2016@gmail.com>
2023-11-07 04:11:16 -0800

    task4 changed utf-8 to res.content

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     res = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(res.content)))
-    print("\t- content: {}".format(res.content.decode('utf-8')))
+    print("\t- content: {}".format(res.content))
