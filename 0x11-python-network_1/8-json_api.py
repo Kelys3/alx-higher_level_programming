9d537c844dad05566204f0566c50aca369d91622
Kelys3 <fredatd2016@gmail.com>
2023-11-07 19:54:25 -0800

    task8edited

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,14 +4,11 @@ http://0.0.0.0:5000/search_user with the letter as parameter"""
 import sys
 import requests
 
+
 if __name__ == "__main__":
-    url = ('http://0.0.0.0:5000/search_user')
+    url = 'http://0.0.0.0:5000/search_user'
 
-    if len(sys.argv) == 1:
-        letter = ""
-    else:
-        letter = sys.argv[1]
-
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
     payload = {'q': letter}
 
     res = requests.post(url, data=payload)
