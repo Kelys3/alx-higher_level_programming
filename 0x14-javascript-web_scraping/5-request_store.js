446453f10ada85cde2aa70e881946f1868dcbcde
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-11-28 07:46:12 -0800

    write to file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filepath = process.argv[3];
+
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
